c7b1590ceda3ad0d010dddd1c8a0661237c31409
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2023-11-17 05:29:39 -0500

    Added containers, components, profile pic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
