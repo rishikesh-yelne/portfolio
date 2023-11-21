ca93ca71923c9fc4aead69a367b6cd275e71b18c
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2023-11-21 02:40:33 -0500

    profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	open(link: string) {
+		switch (link) {
+			case "linkedin":
+				window.open("https://www.linkedin.com/in/rishikesh-yelne/", "_blank");
+				break;
+			case "github":
+				window.open("https://github.com/rishikesh-yelne", "_blank");
+				break;
+			case "resume":
+				window.open("https://drive.google.com/file/d/1ktvtgZLsOGyhjcOuGg3Z7ACdRW-5Klgq/view?usp=sharing", "_blank");
+				break;
+		}
+	}
 }
